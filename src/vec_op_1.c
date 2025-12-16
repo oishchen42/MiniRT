2733ac5acb7447802a32ea0aa008dbb4f5bdf69e
Timothee Dietz Romero <tdietz-r@3-A-18.42heilbronn.de>
2025-12-16 21:06:22 +0100

    norm

--- src/vec_op_1.c
+++ src/vec_op_1.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   vec_op.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oishchen <oishchen@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tdietz-r <tdietz-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 22:11:13 by oishchen          #+#    #+#             */
-/*   Updated: 2025/12/08 22:34:03 by oishchen         ###   ########.fr       */
+/*   Updated: 2025/12/16 16:13:39 by tdietz-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_vcpnt	vec_add(t_vcpnt *vec1, t_vcpnt *vec2)
 {
-	t_vcpnt res;
+	t_vcpnt	res;
 
 	res.vp[0] = vec1->vp[0] + vec2->vp[0];
 	res.vp[1] = vec1->vp[1] + vec2->vp[1];
@@ -25,7 +25,7 @@ t_vcpnt	vec_add(t_vcpnt *vec1, t_vcpnt *vec2)
 
 t_vcpnt	vec_subs(t_vcpnt *vec1, t_vcpnt *vec2)
 {
-	t_vcpnt res;
+	t_vcpnt	res;
 
 	res.vp[0] = vec1->vp[0] - vec2->vp[0];
 	res.vp[1] = vec1->vp[1] - vec2->vp[1];
@@ -36,7 +36,7 @@ t_vcpnt	vec_subs(t_vcpnt *vec1, t_vcpnt *vec2)
 
 t_vcpnt	vec_muls(t_vcpnt *vec1, t_vcpnt *vec2)
 {
-	t_vcpnt res;
+	t_vcpnt	res;
 
 	res.vp[0] = vec1->vp[0] * vec2->vp[0];
 	res.vp[1] = vec1->vp[1] * vec2->vp[1];
@@ -47,10 +47,10 @@ t_vcpnt	vec_muls(t_vcpnt *vec1, t_vcpnt *vec2)
 
 double	vec_mag(t_vcpnt *vec)
 {
-	double mag;
+	double	mag;
 
-	mag = sqrt(pow(vec->vp[0], 2) + pow(vec->vp[1], 2) +
-	pow(vec->vp[2], 2));
+	mag = sqrt(pow(vec->vp[0], 2) + pow(vec->vp[1], 2)
+			+ pow(vec->vp[2], 2));
 	return (mag);
 }
 
@@ -61,63 +61,10 @@ t_vcpnt	vec_norm(t_vcpnt *vec)
 
 	mag = vec_mag(vec);
 	if (mag == 0)
-		return (*vec); 
+		return (*vec);
 	res.vp[0] = vec->vp[0] / mag;
 	res.vp[1] = vec->vp[1] / mag;
 	res.vp[2] = vec->vp[2] / mag;
 	res.vp[3] = vec->vp[3];
-	return (res);
-}
-
-t_vcpnt	vec_scale(t_vcpnt *vec, double scale)
-{
-	t_vcpnt res;
-
-	res.vp[0] = vec->vp[0] * scale;
-	res.vp[1] = vec->vp[1] * scale;
-	res.vp[2] = vec->vp[2] * scale;
-	res.vp[3] = 0;
-	//res.vp[3] = vec->vp[3] * scale;
-	return (res);
-}
-
-void	vec_div(t_vcpnt *vec, double divisor)
-{
-	vec->vp[0] = vec->vp[0] / divisor;
-	vec->vp[1] = vec->vp[1] / divisor;
-	vec->vp[2] = vec->vp[2] / divisor;
-	//vec->vp[3] = vec->vp[3] / divisor;
-}
-
-double	vec_dot(t_vcpnt *vec1, t_vcpnt *vec2)
-{
-	double res;
-
-		res = (vec1->vp[0] * vec2->vp[0]) +
-			(vec1->vp[1] * vec2->vp[1]) +
-			(vec1->vp[2] * vec2->vp[2]);
-	return (res);
-}
-
-t_vcpnt	vec_cross(t_vcpnt *vec1, t_vcpnt *vec2)
-{
-	t_vcpnt res;
-
-	res.vp[0] = vec1->vp[1] * vec2->vp[2] - vec1->vp[2] * vec2->vp[1];
-	res.vp[1] = vec1->vp[2] * vec2->vp[0] - vec1->vp[0] * vec2->vp[2];
-	res.vp[2] = vec1->vp[0] * vec2->vp[1] - vec1->vp[1] * vec2->vp[0];
-	res.vp[3] = 0;
-	return (res);
-}
-
-t_vcpnt	vec_reflect(t_vcpnt *in, t_vcpnt *norm)
-{
-	double	vecs_dt;
-	t_vcpnt	vecs_dt2;
-	t_vcpnt	res;
-
-	vecs_dt = vec_dot(in, norm);
-	vecs_dt2 = vec_scale(norm, vecs_dt * 2.0);
-	res = vec_subs(in, &vecs_dt2);
 	return (res);
 }
